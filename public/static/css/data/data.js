9f3d56477e901f8fa459df20107846aed3699776
zph <767059157@qq.com>
2016-11-01 09:06:25 +0800

    first commit

---
+++ public/static/css/data/data.js
@@ -0,0 +1,33 @@
+$(function () {
+
+    var currYear = (new Date()).getFullYear();
+    var opt={};
+    opt.date = {preset : 'date'};
+    opt.datetime = {preset : 'datetime'};
+    opt.time = {preset : 'time'};
+    opt.default = {
+        theme: 'android-ics light', //皮肤样式
+        display: 'modal', //显示方式
+        mode: 'scroller', //日期选择模式
+        dateFormat: 'yyyy-mm-dd',
+        lang: 'zh',
+        showNow: true,
+        nowText: "今天",
+        startYear: currYear - 1900, //开始年份
+        endYear: currYear + 2000 //结束年份
+    };
+    $("#appDate").mobiscroll($.extend(opt['date'], opt['default']));
+    var optDateTime = $.extend(opt['datetime'], opt['default']);
+    var optTime = $.extend(opt['time'], opt['default']);
+    $("#appDateTime").mobiscroll(optDateTime).datetime(optDateTime);
+    $("#appTime").mobiscroll(optTime).time(optTime);
+    //获取当前的时间
+    function today(){
+        var today=new Date();
+        var h=today.getFullYear();
+        var m=today.getMonth()+1;
+        var d=today.getDate();
+        return h+"-"+m+"-"+d;
+    }
+    document.getElementById("appDate").value = today();
+});
