d807d5eac2e8316d264f849d5dd8821f416d98a3
SVector <s_vxv_s@163.com>
2016-11-02 10:34:33 +0800

    三组

---
+++ application/index/controller/Test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SVector
+ * Date: 2016.11.1
+ * Time: 20:10
+ */
+
+namespace app\index\controller;
+
+
+class Test extends Common
+{
+    public function onlineTest()
+    {
+        return view('onlineTest');
+    }
+}
