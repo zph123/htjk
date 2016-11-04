8a78fd75d4d694f4b819010058b74950e25314b8
刘锦龙 <1099587627@qq.com>
2016-11-04 10:14:06 +0800

    后台继承更新

--- admin/index/controller/Common.php
+++ admin/index/controller/Common.php
@@ -8,10 +8,6 @@ use think\Cookie;
 class Common extends Controller
 {
 	function _initialize(){
-		$controller=request()->controller();            //控制器
-        $action=request()->action();                    //方法
-        Cookie::set('controller', $controller);
-        Cookie::set('action', $action);
        // var_dump(Session::has('username'));die;
 		if(empty(Session::has('username'))){
             $this->redirect("login/login");
