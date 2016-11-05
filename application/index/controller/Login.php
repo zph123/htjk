fb941a3d08b786243ef8111f5bb6db3b8687be1a
Your Name <you@example.com>
2016-11-05 11:53:45 +0800

    updata

--- application/index/controller/Login.php
+++ application/index/controller/Login.php
@@ -14,22 +14,22 @@ class Login extends Controller
 	}
     //执行登录
     public function login(){
-           $today=strtotime(date("Y-m-d 23:59:59"));
-           $u_name = Request::instance()->post('u_name');
-           $checkbox_mini = Request::instance()->post('checkbox_mini');
-           $password = Request::instance()->post('password');
-           $arr = Db::table('gl_users')->where('name', $u_name)->where('password', $password)->find();
-           if (!empty($arr)) {
-               if (!empty($checkbox_mini)) {
-                   Cookie::set('uid', md5($arr['id']), $today-time());
-                   Session::set('uid', $arr['id']);
-               } else {
-                   Session::set('uid', $arr['id']);
-             }
-                  echo 1;
-               } else {
-                  echo 2;
-               }
+       $today=strtotime(date("Y-m-d 23:59:59"));
+       $u_name = Request::instance()->post('u_name');
+       $checkbox_mini = Request::instance()->post('checkbox_mini');
+       $password = Request::instance()->post('password');
+       $arr = Db::table('gl_users')->where('name', $u_name)->where('password', $password)->find();
+       if (!empty($arr)) {
+           if (!empty($checkbox_mini)) {
+               Cookie::set('uid', md5($arr['id']), $today-time());
+               Session::set('uid', $arr['id']);
+           } else {
+               Session::set('uid', $arr['id']);
+         }
+              echo 1;
+           } else {
+              echo 2;
+           }
     }
     //登录后跳转页面
     public function add(){
