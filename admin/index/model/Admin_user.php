235e224e88d39af1e602c7fa4049a7c58058d2d3
刘锦龙 <1099587627@qq.com>
2016-11-05 11:04:19 +0800

    密码设置模型层

--- admin/index/model/Admin_user.php
+++ admin/index/model/Admin_user.php
@@ -11,6 +11,24 @@ class Admin_user extends Model
         $arr =  Db::table('admin_user')->where('u_name', $admin_name)->where('u_pwd', $pwd)->find();
        return $arr;
     }
+    /*密码展示*/
+    function sel_one($username)
+    {
+        $arr=Db::table('admin_user')->where('u_name',$username)->select();
+        return $arr;
+    }
+    /*验证原密码*/
+    function sel_password($username,$pwd)
+    {
+        $arr=Db::table('admin_user')->where('u_name',$username)->where('u_pwd',$pwd)->select();
+        return $arr;
+    }
+    /*修改成功重新登录*/
+    function save_password($passwd,$username)
+    {
+        $arr=Db::table('admin_user')->where('u_name', $username)->update(['u_pwd' => $passwd]);
+        return $arr;
+    }
 
 }
 ?>
