fb822bdb7688b0bb6ed724a1cac61f33c0129665
hanier <1097818457@qq.com>
2016-11-06 20:20:03 +0800

    diet update

--- admin/index/controller/Diet.php
+++ admin/index/controller/Diet.php
@@ -26,12 +26,14 @@ class Diet extends Common
         ->where('is_pay','like',"%$is_pay%")
         ->order("n.add_time desc")
         ->field('n.n_id,n.n_number,n.stuatus,n.add_time,n.is_pay,g.name')
-        ->select();
+        ->paginate(10);
+        $page = $order->render();
         $this->assign('name',$name);
         $this->assign('n_number',$n_number);
         $this->assign('is_pay',$is_pay);
         $this->assign('stuatus',$stuatus);
         $this->assign('order',$order);
+        $this->assign('page',$page);
         return view('index');
     }
 
@@ -47,43 +49,57 @@ class Diet extends Common
 
     public function dietdetails()
     {
-    	//获取答卷ID
-    	$id = Request::instance()->param('id','','strip_tags,strtolower');
-    	$data = Db::field('u_answer,u_desc')->table('user_answer')->where('an_id',$id)->find();
-    	$answer = json_decode($data['u_answer'],true);
-    	$str = '';
-    	foreach ($answer as $key => $val) {
-    		if(is_array($val))
-    		{
-    			foreach ($val as $k => $v) {
-    				$str.=$v.',';
-    			}
-    		}
-			else
-			{
-				$str .= $val.',';
-			}
+
+        //获取答卷ID
+        $id = Request::instance()->param('id','','strip_tags,strtolower');
+        $data = Db::table('nutrition_order')
+        ->alias('n')
+        ->join('gl_users g ','n.u_id = g.id ')
+        ->where('n_id',$id)
+        ->order("n.add_time desc")
+        ->field('n.n_id,n.n_number,n.stuatus,n.add_time,n.is_pay,n.desc,g.name')
+        ->find();
+        $data['desc'] = json_decode($data['desc'],true);
+
+        $this->assign("data",$data);
+        return view('dietdetails');
+   //  	//获取答卷ID
+   //  	$id = Request::instance()->param('id','','strip_tags,strtolower');
+   //  	$data = Db::field('u_answer,u_desc')->table('user_answer')->where('an_id',$id)->find();
+   //  	$answer = json_decode($data['u_answer'],true);
+   //  	$str = '';
+   //  	foreach ($answer as $key => $val) {
+   //  		if(is_array($val))
+   //  		{
+   //  			foreach ($val as $k => $v) {
+   //  				$str.=$v.',';
+   //  			}
+   //  		}
+			// else
+			// {
+			// 	$str .= $val.',';
+			// }
     		
-    	}
-    	$str=rtrim($str,',');
-    	$details = Db::field('q.q_id,q.q_content,a.a_content,GROUP_CONCAT(a.a_content) as a_content')->table('question_answer')->alias('qa')->join('question q','qa.q_id = q.q_id')->join('answer a','qa.a_id = a.a_id')->where('qa.qa_id','in',"$str")->group('q.q_id')->select();
-    	//问答题
-    	if($data['u_desc']!="")
-        {
-            $desc = json_decode($data['u_desc'],true);
-            $str ="";
-            foreach ($desc as $key => $val) {
-                $str .= $key.',';
-            }
-            $desc_question = Db::field('q_id,q_content')->table('question')->alias('q')->where('q.q_id','in',"$str")->select();
-            foreach ($desc_question as $key => $val) {
-                $desc_question[$key]['a_content'] = $desc[$val['q_id']];
-            }
-            $this->assign("desc_question",$desc_question);
-        }
+   //  	}
+   //  	$str=rtrim($str,',');
+   //  	$details = Db::field('q.q_id,q.q_content,a.a_content,GROUP_CONCAT(a.a_content) as a_content')->table('question_answer')->alias('qa')->join('question q','qa.q_id = q.q_id')->join('answer a','qa.a_id = a.a_id')->where('qa.qa_id','in',"$str")->group('q.q_id')->select();
+   //  	//问答题
+   //  	if($data['u_desc']!="")
+   //      {
+   //          $desc = json_decode($data['u_desc'],true);
+   //          $str ="";
+   //          foreach ($desc as $key => $val) {
+   //              $str .= $key.',';
+   //          }
+   //          $desc_question = Db::field('q_id,q_content')->table('question')->alias('q')->where('q.q_id','in',"$str")->select();
+   //          foreach ($desc_question as $key => $val) {
+   //              $desc_question[$key]['a_content'] = $desc[$val['q_id']];
+   //          }
+   //          $this->assign("desc_question",$desc_question);
+   //      }
     	
-    	$this->assign("details",$details);
-    	return view('dietdetails');
+   //  	$this->assign("details",$details);
+   //  	return view('dietdetails');
     }
 
 }
