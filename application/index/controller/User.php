88cead40b2b07814295e9fc3f58f177ea82966a8
Shuai <330207559@qq.com>
2016-11-09 10:08:26 +0800

    支付修改

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -95,8 +95,10 @@ class User extends Common
         ->find();
         if($data){
             if($data['is_pay']==0){
-                $data=file_get_contents("http://www.zphteach.com/htjk/WxpayAPI_php_v3/example/jsapi.php?out_trade_no=$data[out_trade_no]");
-                var_dump($data);
+                echo '123';
+                $this->redirect('http://www.zphteach.com/htjk/WxpayAPI_php_v3/example/jsapi.php?out_trade_no=$data[out_trade_no]');
+                // $data=file_get_contents("http://www.zphteach.com/htjk/WxpayAPI_php_v3/example/jsapi.php?out_trade_no=$data[out_trade_no]");
+                // var_dump($data);
             }else{
                   echo 'is_pay  1'; 
             }            
