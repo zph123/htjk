b90febb3489693bc4866bec158abb1468e729452
刘锦龙 <1099587627@qq.com>
2016-11-18 11:05:08 +0800

    3

---
+++ admin/index/model/Below_list.php
@@ -0,0 +1,30 @@
+<?php
+namespace app\index\model;
+use think\Model;
+use think\Db;
+class Below_list extends Model
+{
+    //添加活动
+    function activity_add($data)
+    {
+
+        return Db::table('below_list')->insert($data);
+
+    }
+    //查询所有活动
+    function activity_select(){
+
+        return Db::table('below_list')->order('l_id DESC')->select();
+    }
+    //活动删除
+    function activity_del($l_id){
+
+        return Db::table('below_list')->delete($l_id);
+    }
+    //修改状态
+    function activity_save($l_id,$dispose){
+
+        return Db::table('below_list')->where('l_id',$l_id)->update(['l_status'=>$dispose]);
+    }
+
+}
